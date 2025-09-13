d3a022228ce0b1c8628087f860d4f62d644fe917
Pavel Kasianov <p.kasianov@gmail.com>
2025-09-13 15:00:47 +0300

    Added icon for content view for temporary nodes

---
+++ src/Hook/Views.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\node_temporary\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Render\Markup;
+use Drupal\node\NodeInterface;
+use Drupal\node_temporary\NodeTemporaryHelper;
+
+/**
+ * Hook implementations for the Node Temporary module.
+ */
+class Views {
+
+  public function __construct(
+    private NodeTemporaryHelper $helper,
+  ) {
+  }
+
+  /**
+   * Implements hook_preprocess_views_view_field().
+   */
+  #[Hook('preprocess_views_view_field')]
+  public function entityDelete(&$variables): void {
+    if ($variables['field']->field == 'title') {
+      if (!empty($variables['row']->_entity) && $variables['row']->_entity instanceof NodeInterface) {
+        $entity = $variables['row']->_entity;
+
+        if ($this->helper->getTemporaryEntity($entity)) {
+          $variables['#attached']['library'][] = 'node_temporary/icon';
+          $variables['output'] = Markup::create($variables['output'] . '<i class="icon-temporary" style="color: orange"></i>');
+        }
+      }
+
+    }
+  }
+
+}
