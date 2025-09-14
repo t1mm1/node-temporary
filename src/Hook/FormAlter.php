c9002c8d3491f03484de510bcd0e450f56c24377
Pavel Kasianov <p.kasianov@gmail.com>
2025-09-15 00:49:16 +0300

    Added validation for date field

--- src/Hook/FormAlter.php
+++ src/Hook/FormAlter.php
@@ -140,11 +140,14 @@ class FormAlter {
 
     $form['node_temporary_options']['date_expire'] = [
       '#type' => 'date',
-      '#description' => $this->t('Expiration date'),
+      '#description' => $this->t('Select expiration date. The date cannot be today or earlier.'),
       '#default_value' => substr($date_value, 0, 10),
       '#disabled' => empty($form['#disabled']) ? 0 : 1,
       '#states' => [
         'visible' => [
+          ':input[name="selected"]' => ['checked' => TRUE],
+        ],
+        'required' => [
           ':input[name="selected"]' => ['checked' => TRUE],
         ],
       ],
@@ -152,9 +155,30 @@ class FormAlter {
 
     foreach (array_keys($form['actions']) as $action) {
       if ($action !== 'preview' && isset($form['actions'][$action]['#type']) && $form['actions'][$action]['#type'] === 'submit') {
+        $form['actions'][$action]['#validate'][] = [self::class, 'validateDateExpireField'];
         $form['actions'][$action]['#submit'][] = [self::class, 'nodeTemporaryFormSubmit'];
       }
     }
+  }
+
+  /**
+   * Help function for validate advanced form section.
+   */
+  public static function validateDateExpireField(array &$form, FormStateInterface $form_state): void {
+    if ($form_state->getValue('selected')) {
+      $date_expire = $form_state->getValue('date_expire');
+      if (empty($date_expire)) {
+        $form_state->setErrorByName('date_expire', t('Expiration date is required when marking node as temporary.'));
+      }
+      else {
+        $date_min = (new \DateTime('now', new \DateTimeZone('UTC')))->modify('+1 day')->format('Y-m-d');
+        if ($date_expire <= $date_min) {
+          $form_state->setErrorByName('date_expire', t('The selected expiration date cannot be today or earlier. Please select a date in the future.'));
+        }
+      }
+    }
+
+    $form_state->setTemporaryValue('entity_validated', TRUE);
   }
 
   /**
